62fd29efc0268475731fc3f2197f843a578798d3
ztr <1621456391@sjtu.edu.cn>
2024-05-11 21:09:54 +0800

    new layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,10 +5,12 @@ function App() {
     <>
     <div className='irregular-box box1'>logo与标题</div>
     <div className='irregular-box box2'>地图框</div>
-    <div className='irregular-box box3'>馆藏与词云</div>
-    <div className='irregular-box box4'>生产年代</div>
-    <div className='irregular-box box5'>来源地热度</div>
-    <div className='irregular-box box6'>文物列表</div>
+    <div className='irregular-box box3'>藏品展示</div>
+    <div className='irregular-box box4'>馆藏</div>
+    <div className='irregular-box box5'>词云</div>
+    <div className='irregular-box box6'>生产年代</div>
+    <div className='irregular-box box7'>图表展示窗口</div>
+    <div className='irregular-box box8'>文物随机对比</div>
     </>
   );
 }
