b17a141c6fe2004f38fc47a3810ddea864b0958a
ztr <1621456391@sjtu.edu.cn>
2024-05-19 16:13:35 +0800

    Gallery finished

--- src/Window.js
+++ src/Window.js
@@ -10,6 +10,7 @@ function Window({ image, onClose }) {
       <div className="modal-content">
         <h2>{image.alt}</h2>
         <img src={image.src} alt={image.alt} className="modal-image" />
+        <p>作者：{image.author}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;来源地：{image.source}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;年代：{image.period}</p>
         <p>{image.description}</p>
         <button onClick={onClose} className="close-button">Close</button>
       </div>
