0900755928def1b3fd14514f5ed32ede734d0b0a
ztr <1621456391@sjtu.edu.cn>
2024-05-29 14:26:08 +0800

    Window improved

--- src/Window.js
+++ src/Window.js
@@ -11,7 +11,7 @@ function Window({ image, onClose }) {
         <h2>{image["alt"]}</h2>
         <img src={image["src"]} alt={image["alt"]} className="modal-image" />
         <p>作者：{image["作者"]}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;来源地：{image["来源地"]}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;年代：{image["年代"]}</p>
-        <p>描述：{image["描述_ZN"]}</p>
+        <p className='description'>描述：{image["描述_ZN"]}</p>
         <button onClick={onClose} className="close-button">Close</button>
       </div>
     </ReactModal>
