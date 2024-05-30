1e5fcd3674b7304aec29de8f7221e0b16be333c7
Abuuu122 <920133490@qq.com>
2024-05-30 20:56:55 +0800

    combine wordcloud

--- src/collection.js
+++ src/collection.js
@@ -2,86 +2,94 @@ import React, { useEffect, useState, useRef } from 'react';
 import ReactEcharts from 'echarts-for-react';
 import './collection.css'
 
-const Collection = ({ museumName}) => {
-  const [pieData, setPieData] = useState([]);
+const Collection = ({ museumName }) => {
+    const [pieData, setPieData] = useState([]);
 
-  const sortedPieData = Object.entries(pieData)
-      .sort((a, b) => b[1] - a[1])
-      .map(([country, count]) => ({ name: country, value: count }));
+    const sortedPieData = Object.entries(pieData)
+        .sort((a, b) => b[1] - a[1])
+        .map(([country, count]) => ({ name: country, value: count }));
+    const topTenPieData = sortedPieData.slice(0, 10);
+    const otherPieData = sortedPieData.slice(10);
 
-  const pieOption = {
-      tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b}: {c} ({d}%)'
-      },
-      legend: {
-          orient: 'vertical',
-          left: 10,
-          data: sortedPieData.map(({ name }) => name)
-      },
-      series: [
-          {
-              name: 'Country',
-              type: 'pie',
-              radius: ['50%', '70%'],
-              avoidLabelOverlap: false,
-              label: {
-                  show: false,
-                  position: 'center'
-              },
-              emphasis: {
-                  label: {
-                      show: true,
-                      fontSize: '30',
-                      fontWeight: 'bold'
-                  }
-              },
-              labelLine: {
-                  show: false
-              },
-              data: sortedPieData
-          }
-      ]
-  };
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-          const response = await fetch("http://localhost:5000/get_data");
-          const data = await response.json();
+    const otherCount = otherPieData.reduce((total, { value }) => total + value, 0);
+    const otherData = { name: '其他', value: otherCount };
 
-          // Filter the data based on the museum name
-          const filteredData = data.filter((item) => item['博物馆'] === museumName);
+    const finalPieData = [...topTenPieData, otherData];
 
-          const counts = filteredData.length;
-          console.log('Total number of items: ', counts);
+    const pieOption = {
+        tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b}: {c} ({d}%)'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 10,
+            data: finalPieData.map(({ name }) => name)
+        },
+        series: [
+            {
+                name: 'Country',
+                type: 'pie',
+                radius: ['50%', '70%'],
+                avoidLabelOverlap: false,
+                label: {
+                    show: false,
+                    position: 'center'
+                },
+                emphasis: {
+                    label: {
+                        show: true,
+                        fontSize: '30',
+                        fontWeight: 'bold'
+                    }
+                },
+                labelLine: {
+                    show: false
+                },
+                data: finalPieData
+            }
+        ]
+    };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://localhost:5000/get_data");
+                const data = await response.json();
 
-          const countryCounts = filteredData.reduce((counts, item) => {
-              const country = item['country'];
-              counts[country] = (counts[country] || 0) + 1;
-              return counts;
-          }, {});
+                // Filter the data based on the museum name
+                const filteredData = data.filter((item) => item['博物馆'] === museumName);
 
-          delete countryCounts['——'];
-          delete countryCounts['null'];
+                const counts = filteredData.length;
+                console.log('Total number of items: ', counts);
 
-          setPieData(countryCounts)
+                const countryCounts = filteredData.reduce((counts, item) => {
+                    const country = item['country'];
+                    counts[country] = (counts[country] || 0) + 1;
+                    return counts;
+                }, {});
 
-          console.log('Country counts: ', countryCounts);
+                delete countryCounts['——'];
+                delete countryCounts['null'];
+
+                setPieData(countryCounts)
+
+                console.log('Country counts: ', countryCounts);
 
 
-      } catch (error) {
-          console.error('Error fetching data: ', error);
-      }
-  };
+            } catch (error) {
+                console.error('Error fetching data: ', error);
+            }
+        };
 
-  fetchData();
-}, [museumName]);
+        fetchData();
+    }, [museumName]);
 
-return (
-  <div>
-      <div className='title'>藏品来源国家</div>
-      <ReactEcharts option={pieOption} />
-  </div>
-)}
+    return (
+        <div>
+            <div className='title'>藏品来源国家</div>
+            <ReactEcharts option={pieOption} />
+        </div>
+    )
+}
 
 export default Collection;
